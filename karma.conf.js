1f38f6da69070213a518cc0af125d84a74cd0b11
ivyabe <abeivybabe@gmail.com>
2020-03-31 16:58:11 +0800

    Updated. Test is now running in Chrome.

--- karma.conf.js
+++ karma.conf.js
@@ -8,15 +8,22 @@ module.exports = function(config) {
         preprocessors: {
             "./src/*.ts": ["karma-typescript"]
         },
-        reporters: ["html", "karma-typescript"],
+        reporters: ["progress", "html", "karma-typescript"],
         htmlReporter: {
             outputFile: "./test/test-results.html"
         },
+        mime: {
+            "text/x-typescript": ["ts", "tsx"]
+        },
         port: 9876,  // karma web server port
         colors: true,
+        client: {
+            clearContext: false
+        },
         logLevel: config.LOG_INFO,
         browsers: ["Chrome", "IE"],
         autoWatch: false,
+        concurrency: Infinity,
         singleRun: true
     });
 };
--- test/src-test.ts
+++ test/src-test.ts
@@ -1,23 +1,50 @@
-describe('Calculate', function() {
+describe('Add, Subtract, Divide, Multiply', function() {
   
-  it ("Add", function() {
+  it ("will add", function() {
     var result = addNumber(10, 5);
     expect(result).toBe(15);
   });
 
-  it ("Subtract", function() {
+  it ("will subtract", function() {
     var result = subtract(10, 5);
     expect(result).toBe(5);
   });
 
-  it ("Divide", function() {
+  it ("will divide", function() {
     var result = divide(10, 5);
     expect(result).toBe(2);
   });
 
-  it ("Multiply", function() {
+  it ("will multiply", function() {
     var result = multiply(10, 5);
     expect(result).toBe(50);
+  });
+
+});
+
+describe('Random', function() {
+
+  it('should return the correct value when two numbers are added', function() {
+    var result = 1 + 1;
+    expect(result).toBe(2);
+  });
+
+});
+
+describe('Testzzz', function() {
+
+  it('should add numbers when a > b', function() {
+    testzzz(2, 5);
+    expect(addNumber).toHaveBeenCalled;
+  });
+
+  it('should subtract numbers when a < b', function() {
+    testzzz(5, 2);
+    expect(subtract).toHaveBeenCalled;
+  });
+
+  it('should return a string when a = b', function() {
+    expect(testzzz(2, 2)).toBe("Same");
   });
 
 });
