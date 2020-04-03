af32e78329819805820507ee8990aab2b1af89ff
ivyabe <abeivybabe@gmail.com>
2020-04-03 09:41:19 +0800

    Modified

--- src/spy-on.ts
+++ src/spy-on.ts
@@ -8,5 +8,9 @@ function Numbers() {
 
     this.getNextNum = function() {
         return this.nextNumber();
+    },
+
+    this.getRandomNum = function(r) {
+        return Math.random() * r;
     }
 }
--- test/spy-on.ts
+++ test/spy-on.ts
@@ -5,27 +5,43 @@ describe("Spy on numbers", function(){
     num = new Numbers();
   });
 
-  it ("Test callThrough()", function() {
+  it("Test callThrough()", function() {
     //callThrough() to make the spy call th actual method
     spyOn(num, "nextNumber").and.callThrough();
     num.getNextNum();
     expect(num.number).toEqual("Two");
   });
 
-  it ("Test returnValue()", function() {
-    //returnValue() to return fixed value without calling
-    //the actual methods
+  it("Test returnValue()", function() {
+    //returnValue() to return fixed value without calling the actual methods
     spyOn(num, "nextNumber").and.returnValue("Three");
-    num.getNextNum();
     expect(num.nextNumber()).toEqual("Three");
   });
 
-  it ("Test callFake()", function() {
+  it("Test callFake()", function() {
     //callFake() to replace the entire spied method
     spyOn(num, "nextNumber").and.callFake(function() {
       return "Four";
     });
-    num.getNextNum();
     expect(num.nextNumber()).not.toEqual("Two");
   });
+
+  it("Test toHaveBeenCalled()", function() {
+    spyOn(num, "nextNumber");
+    num.nextNumber();
+    //toHaveBeenCalled() to verify if spied method is called
+    expect(num.nextNumber).toHaveBeenCalled();
+  });
+
+  it("Test toHaveBeenCalledWith()", function() {
+    spyOn(num, "getRandomNum");
+    num.getRandomNum(30);
+    //toHaveBeenCalled() to verify if spied method is called with correct params
+    expect(num.getRandomNum).toHaveBeenCalledWith(30);
+  });
+
+  it("Test toHaveBeenCalledWith()", function() {
+    expect(num.getRandomNum()).not.toBeNull;
+  });
+
 });
